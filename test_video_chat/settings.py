4af975a6ef649392987d9f2894959cfc94026497
ahmedmawardy-chd <ahmedelmawrdy2@gmail.com>
2021-08-29 17:10:59 +0200

    make it better

--- test_video_chat/settings.py
+++ test_video_chat/settings.py
@@ -121,11 +121,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = "/static/"
-STATIC_ROOT = BASE_DIR / "static"
+STATIC_ROOT = BASE_DIR / "staticfiles"
 STATICFILES_DIRS = [
     ("bootstrap", STATIC_ROOT / "bootstrap"),
     ("jquery", STATIC_ROOT / "jquery"),
-    ("fontawsome", STATIC_ROOT / "fontawsome"),
+    # ("fontawsome", STATIC_ROOT / "fontawsome"),
 ]
 
 MEDIA_URL = "/media/"
